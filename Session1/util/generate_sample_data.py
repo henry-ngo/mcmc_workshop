910052ff9cea8cf2dd642a522e5256734605e91c
Henry Ngo <hhkngo@gmail.com>
2019-01-31 00:04:07 -0800

    Updated Session1. Now ready for Feb 8.

--- Session1/util/generate_sample_data.py
+++ Session1/util/generate_sample_data.py
@@ -7,12 +7,12 @@ import numpy as np
 x_val = np.array([0,1,2,6,9])
 
 # Define true values based on a line with slope and intercept
-true_slope = 1.3
-true_intercept = 0.9
+true_slope = 3.2
+true_intercept = 47.4
 true_y_val = true_intercept + true_slope*x_val
 
 # Define measured y_values with Gaussian errors
-y_err = np.array([0.9,0.6,0.3,1.8,1.5])
+y_err = np.array([0.9,0.6,0.3,1.8,1.5])*3.0
 y_val = np.random.normal(loc=true_y_val,scale=y_err)
 
 # Write to file
